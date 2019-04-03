db51179e9d73e13387da212e43eadbeee189576e
haved <krogstie.havard@gmail.com>
2019-04-03 18:03:46 +0200

    Start parsing svg tags

--- SvgKerfer/svgkerfer.py
+++ SvgKerfer/svgkerfer.py
@@ -6,13 +6,13 @@ from filehandler import get_input_output_list
 outdef = "kerfed/%.svg"
 
 parser = ArgParser("svgkerfer [options] <input files>", "Adds kerf adjustments to stokes in an svg")
-colors = DictOption(["-c", "--color"], "hex_code", "kerf", lambda x:int(x, 16), lambda x:float(x), "Adds kerf adjustment to all strokes of the given color")
+#colors = DictOption(["-c", "--color"], "hex_code", "kerf", lambda x:int(x, 16), lambda x:float(x), "Adds kerf adjustment to all strokes of the given color")
 default = NormalOption(["-d", "--default"], ["kerf"], "Adds a default kerf adjustment")
 inverted = NormalOption(["-i", "--invert"], [], "Inverts whats insides and outsides")
 output = NormalOption(["-o", "--output"], ["path"], "Format for output files. Default: '%s'"%(outdef), outdef)
 recursive = NormalOption(["-r", "--recursive"], [], "Allow converting entire directories")
 
-parser.add(colors, default, inverted, output, recursive)
+parser.add(default, inverted, output, recursive)
 parser.take_args(argv[1:])
 
 io = get_input_output_list(parser.get_the_rest(), recursive=recursive.is_set(), output=output.get_option())
